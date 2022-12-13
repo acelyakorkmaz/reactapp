9722fcd181afb153152a7dd38ef1e69a1c7ff999
acelyakorkmaz <acelyakorkmaz61@gmail.com>
2022-12-13 18:19:28 +0300

    lastest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React,{} from 'react';
+import Navbar from './components/Navbar';
+import User from './components/User';
+
+
 import './App.css';
 
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+   <Navbar title = "User App "/>
+   <hr />
+   <User
+    name= "Açelya Korkmaz"
+    sallary="25000"
+    department= "Tecnology"
+    />
+      <User
+   
+   name= "Ahmet Erdal "
+   sallary="50000"
+   department= "Tecnology"
+    />
+   </div>
+    
+    
   );
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'//impt tab
+
+  class User extends Component {
+    state = {
+        isVisible:false
+    }
+//     constructor(props){
+//         super(props);
+//         this.state={
+//           isVisible:false
+//         }
+//     } ctrl-k-c
+
+  render() {
+    
+    //Destructing
+    const{name, department, sallary}=this.props;
+    const{isVisible}=this.state;
+    return (
+      <div className='col-md-8 mb-4'>
+      <div className="card">
+        <div className="card-header d-flex justify-content-between">
+           <h4 className='d-inline'>{name}</h4>
+           <i className="fa fa-trash-o" aria-hidden="true"></i>
+
+        </div>
+        {
+            isVisible ?
+            <div className="card-body">
+            <p className="card-text">Maaş:{sallary}</p>
+            <p className="card-text">Departman:{department}</p>
+            
+        </div>
+        :null
+        }
+        
+      </div>
+      </div>
+    )
+  }
+}
+User.defaultProps={
+    name:"Bilgi Yok",
+    department:"Bilgi yok",
+    sallary:"Bilgi YOK"
+
+
+}
+User.propTypes={
+    name:PropTypes.string.isRequired,
+    sallary:PropTypes.string.isRequired,
+    department:PropTypes.string.isRequired
+
+}
+export default User;
+
